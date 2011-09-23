1d9232ccf79df6de3b35a8a6dcaa3675f3a63f2b
Frank Tagne <ftagne@obeo.fr>
2011-09-23 17:13:17 +0200

    First commit of the project.

---
+++ plugins/org.acceleo.performance/src/org/acceleo/performance/AcceleoPerformanceActivator.java
@@ -0,0 +1,16 @@
+package org.acceleo.performance;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class AcceleoPerformanceActivator extends Plugin {
+
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
+
+}

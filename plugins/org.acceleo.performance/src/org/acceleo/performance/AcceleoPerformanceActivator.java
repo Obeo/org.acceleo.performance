497d192e8886aa63c6c92b5205b9e88124f614a7
Frank Tagne <ftagne@obeo.fr>
2011-09-23 17:21:47 +0200

    Checkstyle

--- plugins/org.acceleo.performance/src/org/acceleo/performance/AcceleoPerformanceActivator.java
+++ plugins/org.acceleo.performance/src/org/acceleo/performance/AcceleoPerformanceActivator.java
@@ -1,14 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.acceleo.performance;
 
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
+/**
+ * The Activator of the plugin.
+ * 
+ * @author <a href="mailto:frank.tagne@obeo.fr">Frank Tagne</a>
+ */
 public class AcceleoPerformanceActivator extends Plugin {
 
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
 	public void stop(BundleContext context) throws Exception {
 		super.stop(context);
 	}
